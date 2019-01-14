a75111117b54d434f683cafc079e3c28b1d2fe7d
Jared Lam <jared.h.lam@gmail.com>
2019-01-14 09:00:28 -0800

    Header file for Mystring section

---
+++ Projects/Project 4/mystring.h
@@ -0,0 +1,6 @@
+#prama once
+
+char * myStringCat(char * destination, const char * source);
+size_t myStringLength(const char * str);
+char * myStringCopy(char * destination, const char * source);
+int myStringCompare(const char *str1, const char *str2);
